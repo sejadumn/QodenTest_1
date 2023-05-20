9c739110af6deead5ef6f88ec6807c9a3e066580
sejadumn <sejadamn@outlook.com>
2023-05-21 05:22:05 +0700

    Removing classes to a separate folder, the reason is rechecking against the requirements

---
+++ QodenTest_1/Class/HashTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QodenTest_1.Class
+{
+    public class HashTable
+    {
+        public List<ListNode>[] values;
+
+        public HashTable(int size)
+        {
+            values = new List<ListNode>[size];
+            for (int i = 0; i < size; i++)
+            {
+                values[i] = new List<ListNode>();
+            }
+        }
+
+        private int HashFunction(int x, int size)
+        {
+            return x % size;
+        }
+
+        public void Insert(int newValue)
+        {
+            int index = HashFunction(newValue, values.Length);
+            if (values[index].Count == 0)
+            {
+                values[index].Add(new ListNode(newValue));
+            }
+            else
+            {
+                values[index][0].Insert(newValue);
+            }
+        }
+
+        public void PrintTable()
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.Write($"m{i}: ");
+                foreach (ListNode node in values[i])
+                {
+                    ListNode currNode = node;
+                    while (currNode != null)
+                    {
+                        Console.Write(currNode.value + " ");
+                        currNode = currNode.next;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ QodenTest_1/Class/ListNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QodenTest_1.Class
+{
+    public class ListNode
+    {
+        public int value;
+        public ListNode next;
+
+        public ListNode(int value)
+        {
+            this.value = value;
+            next = null;
+        }
+
+        public void Insert(int newValue)
+        {
+            ListNode newNode = new ListNode(newValue);
+            newNode.next = next;
+            next = newNode;
+        }
+    }
+}
